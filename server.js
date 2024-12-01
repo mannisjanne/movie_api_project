9417adf8250d73828160c7928aa9ec5f33527436
mannisjanne <n3maja00@students.oamk.fi>
2024-12-01 19:12:10 +0200

    Adding endpoint

--- server.js
+++ server.js
@@ -11,5 +11,11 @@ app.get('/', (req, res) => res.send('Hello world'));
 app.get('/movie', (req, res) => res.send('Movie endpoint'));
 app.get('/review', (req, res) => res.send('Review endpoint'));
 app.get('/favorite', (req, res) => res.send('Favorite endpoint'));
-app.get('/user', (req, res) => res.send('User endpoint'));
+app.get('/users', (req, res) => res.send('Users endpoint'));
 app.get('/genre', (req, res) => res.send('Genre endpoint'));
+
+app.post('/genre', (req, res) =>{
+    let user = req.body;
+    console.log(user);
+    res.send();
+});
